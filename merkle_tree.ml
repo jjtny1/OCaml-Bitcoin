e724035eee34ff18a63a45d82c9832e3873cfa25
Joby Thundil <jjtny1@gmail.com>
2018-06-23 01:07:40 -0400

    extend initial merkle tree implementation

--- merkle_tree.ml
+++ merkle_tree.ml
@@ -1,3 +1,5 @@
+type 'a merkle_tree = { data : 'a list; levels : int list list; root : int}
+
 let hash s1 = Hashtbl.hash s1
 
 let concat s1 s2 = "(" ^ string_of_int s1 ^ ":" ^ string_of_int s2 ^ ")"
@@ -11,34 +13,61 @@ let compute_merkle_level level =
 
 	List.rev (aux [] level)
 
-let merkle_root li =
-	let rec aux level = match (compute_merkle_level level) with
-		| [] -> hash []
-		| [h] -> h
-		| v -> aux v
+let get_sibling index li =
+	let bit, sibling_index = if index mod 2 = 0 then (1, index+1) else (0, index-1) in
+	(bit, List.nth li (min ((List.length li)-1) sibling_index))
+
+let create_merkle_tree li =
+	let rec aux ac level = match level with
+		| [] -> (hash 0, [])
+		| [root] as top -> (root, top :: ac)
+		| v -> aux (level :: ac) (compute_merkle_level level)
 	in
 
 	let hashed_li = List.fold_left (fun ac h -> (hash h) :: ac) [] li in
-	aux (List.rev hashed_li) 
+	let (root, levels) = aux [] (List.rev hashed_li) in
+	{ data = li; levels = List.rev levels; root = root }
 
-let merkle_proof elem_index li =
-	let get_sibling index li =
-		let bit, sibling_index = if index mod 2 = 0 then (1, index+1) else (0, index-1) in
-		(bit, List.nth li (min ((List.length li)-1) sibling_index))
+let add_element merkle_tree elem =
+	let rec replace_elem index elem li = match li with
+		| [] -> [elem]
+		| h :: t -> if (index = 0) then elem :: t else h :: replace_elem (index-1) elem t
 	in
 
-	let rec aux index level ac = 
-		(* Add the sibling of the current level to ac *)
-		let new_ac = (get_sibling index level) :: ac in
-		match (compute_merkle_level level) with
-		| [] | [_] -> new_ac
-		| next_level -> aux (index/2) next_level new_ac
+	let rec aux (new_levels,current_hash,index) level = match level with
+		| [] -> ([], hash [], 0)
+		| _ -> 
+			let new_level = replace_elem index current_hash level in
+			let (side, sibling) = get_sibling index new_level in
+			let new_hash = if side = 0 then hash (concat sibling current_hash) else hash (concat current_hash sibling) in
+			(new_level :: new_levels, new_hash, index/2)
 	in
 
-	let hashed_li = List.fold_left (fun ac h -> (hash h) :: ac) [] li in
-	List.rev (aux elem_index (List.rev hashed_li) [])
+	let rec compress_to_root new_levels = match new_levels with
+		| [] -> new_levels
+		| level :: _ ->
+			match level with
+			| [] | [_] -> new_levels
+			| v -> compress_to_root ((compute_merkle_level level) :: new_levels)
+	in
 
-let merkle_verify elem root proof =
+	let elem_hash = hash elem in
+	let index = (List.length merkle_tree.data) in
+	let (new_levels, root, _) = List.fold_left aux ([], elem_hash, index) merkle_tree.levels in
+	{ data=merkle_tree.data@[elem]; levels = List.rev (compress_to_root new_levels); root=root }
+
+let merkle_proof merke_tree elem_index =
+	let get_level_proof (index, ac) level = match level with
+		| [] -> (0, [])
+		| [root] -> (index, ac)
+		| _ -> (index/2, (get_sibling index level) :: ac)
+	in
+
+	let (_, proof) = List.fold_left get_level_proof (elem_index, []) merke_tree.levels in
+
+	List.rev proof
+
+let merkle_verify elem root proof  =
 	let rec aux ac = function
 		| [] -> ac
 		| (side, s_hash) :: t ->
@@ -48,4 +77,3 @@ let merkle_verify elem root proof =
 
 	let elem_hash = hash elem in
 	root = aux elem_hash proof 
-
